f44740b7af6be7d2003a95433a6bff611592a4f9
bakwandoto <abiyyur0@gmail.com>
2025-07-15 03:50:49 +0700

    Create items.js

---
+++ items.js
@@ -0,0 +1,17 @@
+[
+  {
+    "name": "Wheat Bread",
+    "exp": 200,
+    "cost": 50
+  },
+  {
+    "name": "Pumpkin Pie",
+    "exp": 500,
+    "cost": 120
+  },
+  {
+    "name": "Carrot Juice",
+    "exp": 300,
+    "cost": 90
+  }
+]
